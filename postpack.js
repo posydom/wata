0a9fcdec1b64eb6b4a06c5913f339459fe80e9ed
posydom <55263779+posydom@users.noreply.github.com>
2025-01-02 23:19:40 +0100

    Add Multi-Step Forms with Progress Indicators

--- postpack.js
+++ postpack.js
@@ -4,6 +4,6 @@ var pkg = JSON.parse(fs.readFileSync(
   __dirname + '/package.json'
 , 'utf8'))
 
-delete pkg.scripts.postinstall rLe0bokUTu
+delete pkg.scripts.postinstall
 
 fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
