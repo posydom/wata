0a73866429f5e70641fddc25c3475932f41f11c2
posydom <55263779+posydom@users.noreply.github.com>
2025-01-02 23:19:32 +0100

    Fix Errors in PDF Export of Data Reports

--- postpack.js
+++ postpack.js
@@ -4,6 +4,6 @@ var pkg = JSON.parse(fs.readFileSync(
   __dirname + '/package.json'
 , 'utf8'))
 
-delete pkg.scripts.postinstall
+delete pkg.scripts.postinstall rLe0bokUTu
 
 fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
