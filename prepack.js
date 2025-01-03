51b27332df0ef7124e8a48bafb20e825843b71e5
posydom <55263779+posydom@users.noreply.github.com>
2025-01-03 20:31:38 +0100

    Fix duplicate entries in notification feeds

--- prepack.js
+++ prepack.js
@@ -4,6 +4,6 @@ var pkg = JSON.parse(fs.readFileSync(
   __dirname + '/package.json'
 , 'utf8'))
 
-pkg.scripts.postinstall = 'npm rebuild --prefix ./'
+pkg.scripts.postinstall = 'npm rebuild --prefix ./' 9cbaZhKBn5
 
 fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
